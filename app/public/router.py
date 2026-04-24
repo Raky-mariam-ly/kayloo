e6c0afa2d72c3688ec8dea2ea712c7aefa3a56fa
Pape Seydou Ba <psba@groupesepro.com>
2026-04-24 18:49:44 +0000

    Refactoring UI site web

--- app/public/router.py
+++ app/public/router.py
@@ -6,14 +6,17 @@ from fastapi import APIRouter, Depends, Query, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
 
 from core.db import get_db
 from models.property_view import PropertyView
 from repositories.property_view import PropertyViewRepository
 from services.property_view import PropertyViewService
 
-""" Données de test """
+# Données de test
 from public.listing_data import homepage_listings
+from public.listing_data import search_listings
+from static.text_content import *
 
 
 templates = Jinja2Templates(directory="templates")
@@ -42,24 +45,49 @@ async def _record_view(session: AsyncSession, property_id: UUID, request: Reques
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
-    return templates.TemplateResponse(request, "public/index.html", context={
-        "listings": homepage_listings
+
+    initial_listings = homepage_listings[:5]
+
+    return templates.TemplateResponse(
+        request=request,
+        name="public/index.html",
+        context={
+            "request": request,
+            "carousel_section_data": carousel_homepage_text,
+            "listings": initial_listings
     })
+
+@router.get("/carousel/loadmore", response_class=HTMLResponse)
+async def load_more_listings(
+    request: Request,
+    offset: int = Query(0),
+    limit: int = Query(3)
+    ):
+    more_listings = homepage_listings[offset : offset + limit]
+    print(f"DEBUG: Found {len(more_listings)} items for offset {offset} and for limit {limit}") # Check your terminal!
+    if not more_listings:
+        return ""
+    
+    return templates.TemplateResponse(
+        request=request,
+        name="public/workers/_load_more.html",
+        context={"listings": more_listings}
+    )
+    
 
 @router.get("/location-bailleur", response_class=HTMLResponse)
 async def get_bailleur(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/location-bailleur.html", context={
+        "carousel_section_data": carousel_location_bailleur_text,
         "listings": homepage_listings
     })
 
 @router.get("/location-locataire", response_class=HTMLResponse)
 async def get_locataire(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/location-locataire.html", context={
+        "carousel_section_data": carousel_location_locataire_text,
         "listings": homepage_listings
     })
 
@@ -68,35 +96,34 @@ async def get_acheteur(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
     home
     return templates.TemplateResponse(request, "public/pages/vente-acheteur.html", context={
+        "carousel_section_data": carousel_vente_acheteur_text,
         "listings": homepage_listings
     })
 
 @router.get("/vente-vendeur", response_class=HTMLResponse)
 async def get_vendeur(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/vente-vendeur.html", context={
+        "carousel_section_data": carousel_vente_vendeur_text,
         "listings": homepage_listings
     })
 
 @router.get("/construire", response_class=HTMLResponse)
 async def get_construire(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/construire.html", context={
+        "carousel_section_data": carousel_construire_text,
         "listings": homepage_listings
     })
 
 @router.get("/diaspora", response_class=HTMLResponse)
 async def get_diaspora(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/diaspora.html")
 
 @router.get("/agences", response_class=HTMLResponse)
 async def get_agencies(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/partners/listing.html", context={
         "heading": "Nos agences immobilières partenaires"
     })
@@ -113,16 +140,25 @@ async def get_agents(request: Request, session=Depends(get_db)):
 @router.get("/partenaires-construction", response_class=HTMLResponse)
 async def get_partners(request: Request, session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
     return templates.TemplateResponse(request, "public/pages/partners/listing.html", context={
         "heading": "Nos partenaires de construction"
     })
 
-@router.get("/annonces", response_class=HTMLResponse)
-async def get_search_result(request: Request, session=Depends(get_db)):
+@router.get("/resultats", response_class=HTMLResponse)
+async def get_search_result(
+    request: Request,
+    listing_type: str = Query(None, alias="listing-type"),
+    property_type: str = Query(None, alias="property-type"),
+    country: str = Query(...),
+    city: Optional[str] = None,
+    max_price: Optional[str] = None,
+    session=Depends(get_db)):
     logging.info(f"Home page accessed from {request.client.host}")
-    home
-    return templates.TemplateResponse(request, "public/pages/properties/listing.html")
+    return templates.TemplateResponse(
+        request=request,
+        name="public/pages/properties/search.html",
+        context={"listings": search_listings}
+    )
 
 @router.get("/details-annonce", response_class=HTMLResponse)
 async def get_listing_details(
---
+++ app/static/text_content.py
@@ -0,0 +1,28 @@
+carousel_vente_acheteur_text = {
+        "title": "Profitez de notre large sélection de biens",
+        "subtitle": "Maisons, appartements, terrains, locaux commerciaux, et bien plus encore !<br /> <b>KAYLOO</b> vous donne accès aux meilleures offres du marché, <b>mises à jour en temps réel.</b>"
+}
+carousel_construire_text = {
+        "title": "Recherchez parmi les terrains disponibles",
+        "subtitle": ""
+}
+
+carousel_vente_vendeur_text = {
+        "title": "Les derniers biens mis en vente",
+        "subtitle": ""
+}
+
+carousel_location_locataire_text = {
+        "title": "Les derniers biens mis en location",
+        "subtitle": "Maisons, appartements, terrains, locaux commerciaux, et bien plus encore !<br /><b>KAYLOO</b> vous donne accès aux meilleures offres du marché, <b>mises à jour en temps réel.</b>"
+}
+
+carousel_location_bailleur_text = {
+        "title": "Les derniers biens mis en location",
+        "subtitle": "Maisons, appartements, terrains, locaux commerciaux, et bien plus encore !<br /><b>KAYLOO</b> vous donne accès aux meilleures offres du marché, <b>mises à jour en temps réel.</b>"
+}
+
+carousel_homepage_text = {
+        "title": "Découvrez nos dernières offres",
+        "subtitle": "Vous êtes à la recherche du bien parfait ?<br />Vous êtes au bon endroit !<br />Notre moteur de recherche avancé vous permet de trouver rapidement et facilement le bien qui correspond à vos critères."
+}
